c62e39a30a0e09ac0d78a9c63c54a3fb8c5f684d
undefeatablemon <57371390+undefeatablemon@users.noreply.github.com>
2025-05-13 16:17:24 -0500

    Added Trailblazer's Bat and it's model

--- src/main/java/net/undef/hsr_craft/HSRcraft.java
+++ src/main/java/net/undef/hsr_craft/HSRcraft.java
@@ -1,6 +1,7 @@
 package net.undef.hsr_craft;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -8,24 +9,25 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.undef.hsr_craft.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(HSRcraft.MOD_ID)
-public class HSRcraft
-{
+public class HSRcraft{
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "hsr_craft";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public HSRcraft(FMLJavaModLoadingContext context)
-    {
+    public HSRcraft(FMLJavaModLoadingContext context){
         IEventBus modEventBus = context.getModEventBus();
+
+        //Ensures the items deferred register is properly registered
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -40,31 +42,28 @@ public class HSRcraft
         //context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event){
 
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.COMBAT){
+            event.accept(ModItems.TRAILBLAZERBAT); //Adds items into the ingredients tab in the creative menu
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event){
 
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents{
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event){
 
         }
     }
---
+++ src/main/java/net/undef/hsr_craft/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.undef.hsr_craft.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.undef.hsr_craft.HSRcraft;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HSRcraft.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+    //Trailblazer's Bat
+    public static final RegistryObject<Item> TRAILBLAZERBAT = ITEMS.register("trailblazer_bat", () -> new Item(new Item.Properties()));
+}
